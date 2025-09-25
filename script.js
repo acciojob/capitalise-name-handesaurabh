f503fc49947d5f7ad908c4995704caca08189214
handesaurabh <handesaurabh2004@gmail.com>
2025-09-25 12:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const inputField = document.getElementById("fname");
+
+inputField.addEventListener("blur", function () {
+  this.value = this.value.toUpperCase();
+});
